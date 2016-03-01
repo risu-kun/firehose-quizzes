8ad919ca0c1755bdfdf171fe7119322afd6e921b
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-01 01:47:40 +0000

    Add migration for comments table

--- db/migrate/20160301014446_create_comments.rb
+++ db/migrate/20160301014446_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
 
+			t.string :author
+			t.text :contents
       t.timestamps null: false
     end
   end
